f345a51b52f0675a2bd0c1813688a1562fda60d7
alexanderfewtrell <53369244+alexanderfewtrell@users.noreply.github.com>
2023-09-16 20:24:56 +0100

    added players and winning criteria (#4)

--- Assets/Scripts/MovePlate.cs
+++ Assets/Scripts/MovePlate.cs
@@ -31,6 +31,10 @@ public class MovePlate : MonoBehaviour
         {
             GameObject cp = controller.GetComponent<Game>().GetPosition(matrixX, matrixY);
 
+            if (cp.name == "white_king") controller.GetComponent<Game>().Winner("black");
+
+            if (cp.name == "black_king") controller.GetComponent<Game>().Winner("white");
+
             Destroy(cp);
         }
 
@@ -43,6 +47,8 @@ public class MovePlate : MonoBehaviour
         reference.GetComponent<Chessman>().SetCoords();
 
         controller.GetComponent<Game>().SetPosition(reference);
+
+        controller.GetComponent<Game>().NextTurn();
 
         reference.GetComponent<Chessman>().DestroyMovePlates();
     }
