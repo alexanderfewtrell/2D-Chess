5c8846efb4c5e0675db17b13797c7106608f4e86
alexanderfewtrell <53369244+alexanderfewtrell@users.noreply.github.com>
2024-01-03 17:47:23 +0000

    Feature/splitting moveplates and checkplates (#12)
    * seperated moveplates and checkplates
    
    * seperated white in check and black in check variables and display text

--- Assets/Scripts/GlobalVariables.cs
+++ Assets/Scripts/GlobalVariables.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class GlobalVariables : MonoBehaviour
 {
     //Defines the Global Variables
-
-    public static bool InCheck = false;
+    public static bool WhiteInCheck = false;
+    public static bool BlackInCheck = false;
 
     public static long GameId = 0;
     public static int WhiteScore = 0;
